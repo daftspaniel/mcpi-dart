abb8fd0d05fcc449c9d2634c237e5c1e1a1a656d
daftspaniel <daftspaniel@gmail.com>
2015-06-03 19:12:47 +0100

    Initial public release.

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, Davy Mitchell. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'mcdart.dart';
+import '../lib/mcdart.dart';
 import 'dart:math' as math;
 
 Minecraft mci;
